b553494f7402ad4244574c6ecd4f38c3f08ec66c
Caio Mendes <caio-mendes-@hotmail.com>
2020-03-03 09:00:50 -0300

    primeiro commit

---
+++ tarefas/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="container grid-xs py-2">
+      <h1 class="text-center">Agenda de Tarefas</h1>
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" v-model="todo.descricao" class="form-input" placeholder="Nova Tarefa">
+          <button class ="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+        <div class="todo-list">
+          <todo v-for= "t in todos" :key="t.id" :todo="t" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo'
+
+export default {
+  name: 'App',
+  components:{Todo},
+  data(){
+    return {todos:[], todo:{ checked: false}}
+  },
+  methods: {
+    addTodo(todo){
+      todo.id = Date.now()
+      this.todos.push(todo)
+      this.todo = {checked: false}
+    }
+  },
+}
+</script>
+
+<style>
+.todo-list{
+  padding-top: 2rem;
+}
+</style>
---
+++ tarefas/src/components/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="todo-list">
+        <div class="tile">
+          
+            <div class="tile-icon">
+              <div>
+                 <i class="icon icon-time flex-centered "></i>
+              </div>
+             
+            </div>
+          <div class="tile-content">
+            <p class="tile-subtitle">{{todo.descricao}}</p>
+          </div>
+          <div class="tile-action">
+            <button class= "btn btn-link">Concluído</button>
+            <button class= "btn btn-link">
+              <span class="text-error">Remover</span>
+              </button>
+          </div>
+        </div>
+        </div>
+</template>
+
+<script>
+export default {
+    props:{
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
